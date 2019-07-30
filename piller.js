832a58a7c4b7dc914a934d6048588e8567fc63c2
Satyam Gupta <satyamforwork@gmail.com>
2019-07-30 23:03:24 +0530

    Updated ML GA NN Code

--- piller.js
+++ piller.js
@@ -1,32 +1,30 @@
-function piller(x,pillerbottomImage,pillertopImage){
+function piller(){
     this.pos = {}
     this.height =  floor(random(100, 250))
     this.width = 60
     this.pos.x = 600
     this.pos.y = 400-this.height
-    this.speed = 2
+    this.bottom = this.pos.y
     this.space = 150
+    this.top = this.pos.y - this.space
+    this.speed = 6
     this.pillerbottomImage = pillerbottomImage
     this.pillertopImage = pillertopImage
 
-    this.addFrame = function(){
+    this.update = function(){
         this.pos.x -= this.speed
     }
 
     this.draw = function(){
-        this.addFrame()
-        // fill(60)
-        // rect(this.pos.x, this.pos.y, this.width, this.height)
         for(i = this.pos.y; i < this.pos.y + this.height;i+=3){
             image(pillerbottomImage,this.pos.x, i, this.width);
         }
         image(pillertopImage,this.pos.x, this.pos.y, this.width);
         
 
-        for(i = 0; i+20 < this.pos.y - this.space;i+=20){
+        for(i = 0; i+20 < this.top;i+=20){
             image(pillerbottomImage,this.pos.x, i, this.width);
         }
-        image(pillertopImage,this.pos.x,this.pos.y - this.space - 20, this.width);
-        // rect(this.pos.x, 0, this.width, this.pos.y - this.space)
+        image(pillertopImage,this.pos.x,this.top - 20, this.width);
     }
 }
--- sketch.js
+++ sketch.js
@@ -2,8 +2,10 @@ dim = {
     x: 600,
     y: 400
 }
-var bird1;
+var activeBirds = [];
+var birdsPopulation = 200;
 var pillers = [];
+var allBirds = []
 var isPillerRecentlyMadeTime = 0
 var birdImage
 var pillertopImage
@@ -11,90 +13,93 @@ var pillerbottomImage
 var gameover = false
 var Stopcounter = -100
 var bg
-var score = 0
+var speedBox
 
 function setup() {
-    createCanvas(dim.x, dim.y)
+    var canvas = createCanvas(dim.x, dim.y)
+    canvas.parent("#canvascontainer")
+    
     bg = loadImage("image/background.jpg"); 
     birdImage = loadImage("image/bird.png"); 
     pillertopImage = loadImage("image/pillertop.png")
     pillerbottomImage = loadImage("image/pillerbottom.png")
-    bird1 = new bird(dim.x,dim.y,birdImage)
+    for(i=0;i<birdsPopulation;i++){
+      activeBirds[i] = new Bird(dim.x,dim.y,birdImage)
+      allBirds[i] = activeBirds[i]
+    }
+    speedBox = select('#speed')
 }
 
 function draw() {
   image(bg,0,0,dim.x,dim.y)
-  // background("#76a0ff")
-  if(pillers.length < 4 && isPillerRecentlyMadeTime < 0){
-    p = new piller(1,pillerbottomImage,pillertopImage)
-    pillers.push(p)
-    isPillerRecentlyMadeTime = 100
-  }
-  isPillerRecentlyMadeTime--
-  pillers.forEach(p => {
-      p.draw()
-  });
-
-  pillers.forEach(function(p, index, object){
-    if(p.pos.x == -10){
-      score++
+  timesFaster = select("#speed").value()
+  speedBox.html(timesFaster)
+  for(zzzz=0;zzzz<timesFaster;zzzz++){
+    if(pillers.length < 4 && isPillerRecentlyMadeTime < 0){
+      p = new piller()
+      pillers.push(p)
+      isPillerRecentlyMadeTime = 100
     }
-    if(p.pos.x < -80){
-      object.splice(index,1)
-    }
-  });
-
-
-  setscore()
-  bird1.draw()
-  if(gameover){
-    gameOver()
-  }else{
-    pillers.forEach(piller => {
-      var hit1 = false;
-      var hit2 = false;
-      hit1 = collideRectCircle(piller.pos.x,piller.pos.y,piller.width,piller.height,bird1.pos.x,bird1.pos.y,bird1.hw);
-      hit2 = collideRectCircle(piller.pos.x,0,piller.width,piller.pos.y - piller.space,bird1.pos.x,bird1.pos.y,bird1.hw);
-      if(bird1.pos.y < 0){
-        gameOver();
-        return ""  
+    isPillerRecentlyMadeTime--
+    pillers.forEach(function(p, index, object){
+      if(p.pos.x == -10){
       }
-      if(hit1 || hit2){
-        gameOver();
-        return ""  
+      if(p.pos.x < -80){
+        object.splice(index,1)
       }
     });
+    pillers.forEach(piller => {
+      piller.update()
+    });
+    activeBirds.forEach(bird => {
+      bird.update()
+      bird.think(pillers)
+    });
+    checkCollision() 
+    if (activeBirds.length == 0) {
+      nextGeneration();
+    }
   }
+  
+  pillers.forEach(piller => {
+    piller.draw()
+  });
+  activeBirds.forEach(bird => {
+    bird.draw()
+  });
+
+
 }
 
-function gameOver(){
-  if(!gameover){
-    gameover = true;
-  }
-  fill(0,150)
-  rect(0,0,dim.x,dim.y)
-  fill(255)
-  textAlign(CENTER)
-  textSize(45)
-  textStyle(BOLD);
-  bird1.killed()
-  text("Game Over :(",dim.x/2,dim.y/2);
-  Stopcounter++
-  if(Stopcounter > 0){
-    noLoop()
-  }
-}
 
-function keyPressed() {
-  if(keyCode === 32){
-    bird1.addBounce()
-  }
-}
+// function keyPressed(){
+//   if(keyCode == 32){
+//     activeBirds.forEach(bird => {
+//       bird.up()
+//     });
+//   }
+// }
 
-function setscore(){
-  textAlign(LEFT)
-  textSize(20)
-  textStyle(BOLD);
-  fill(255,0,0)
-  text("Score: "+score,10,30)
+
+function checkCollision(){
+  pillers.forEach(piller => {
+    for(i=0;i<activeBirds.length;i++){
+      b = activeBirds[i]
+      var hit1 = false;
+      var hit2 = false;
+      hit1 = collideRectCircle(piller.pos.x,piller.pos.y,piller.width,piller.height,b.pos.x,b.pos.y,b.hw);
+      hit2 = collideRectCircle(piller.pos.x,0,piller.width,piller.pos.y - piller.space,b.pos.x,b.pos.y,b.hw);
+      // if(b.pos.y < 0){
+      //   // gameOver();
+      //   // return ""  
+      // }
+      if(b.pos.y + b.speed > 400-b.hw/2){
+        activeBirds.splice(i,1)
+      }else if(b.pos.y + b.speed < 0+b.hw/2){
+        activeBirds.splice(i,1)
+      }else if(hit1 || hit2){
+        activeBirds.splice(i,1)
+      }
+    };
+  });
 }
